166c50a254672e9eb947594df4f171898225c58b
kintetsu998 <juliusvega998@gmail.com>
2016-08-01 20:22:09 +0800

    Added initial layout

--- app/src/main/java/com/kintetsu/aspirev3/randomizer/SplashScreenActivity.java
+++ app/src/main/java/com/kintetsu/aspirev3/randomizer/SplashScreenActivity.java
@@ -1,13 +1,27 @@
 package com.kintetsu.aspirev3.randomizer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private static final String TAG = SplashScreenActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Intent i = new Intent(this, MainActivity.class);
+        final Handler handler = new Handler();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
     }
 }
