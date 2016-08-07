ea8a2d512c4bb85a6c417691fdc43a543c156540
kintetsu998 <juliusvega998@gmail.com>
2016-08-08 01:13:06 +0800

    Now randomizes names

--- app/src/main/java/com/kintetsu/aspirev3/randomizer/MainActivity.java
+++ app/src/main/java/com/kintetsu/aspirev3/randomizer/MainActivity.java
@@ -1,13 +1,25 @@
 package com.kintetsu.aspirev3.randomizer;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private final String[] mList = {
+            "Kuya Mark",
+            "Alyssa",
+            "Aleli",
+            "Nur",
+            "Julius"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +28,33 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final Button randomize = (Button) findViewById(R.id.randomize);
+        final TextView result = (TextView) findViewById(R.id.result);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+
+        randomize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Random r = new Random();
+                for(int i=0; i<100; i++) {
+                    final int index = r.nextInt(mList.length);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            result.setText(mList[index]);
+                        }
+                    }, 50*i);
+                }
             }
         });
     }
